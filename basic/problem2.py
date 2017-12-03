3acfaed7c1d31de7dfa6fdbf76dde8c738c19fb6
Tyler C. Julian <tylercjulian@live.com>
2017-12-03 09:56:17 -0500

    Added heading

--- basic/problem2.py
+++ basic/problem2.py
@@ -1,3 +1,14 @@
+# -------------------------------------------------------------------
+# 
+# Excercise from: http://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+#
+# Objectives:
+# 1. Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+# 2. If the number is a multiple of 4, print out a different message.
+# 3. Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.#
+#
+# -------------------------------------------------------------------
+
 def main():
     number = get_number()
     odd_or_even(number)
