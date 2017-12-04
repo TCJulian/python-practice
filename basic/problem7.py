3ce39580097535590868b75afa4a255f4fcbb5dc
Tyler C. Julian <tylercjulian@live.com>
2017-12-04 14:02:23 -0500

    Create problem7.py

---
+++ basic/problem7.py
@@ -0,0 +1,19 @@
+# -------------------------------------------------------------------
+# 
+# Excercise from: http://www.practicepython.org/solution/2014/03/26/07-list-comprehensions-solutions.html
+# 
+# Initial Conditions:
+# a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+#
+# Objectives:
+# 1. Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+#
+# ------------------------------------------------------------------- 
+
+def main():
+    a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    even = [num] for num in a if num % 2 == 0]
+    print(even)
+
+if __name__ == "__main__":
+    main()
