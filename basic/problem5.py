c1c68dcdc63ef45bf45761a796a51bd9a78f9c80
Tyler C. Julian <tylercjulian@live.com>
2017-12-04 13:35:14 -0500

    Create problem5.py

---
+++ basic/problem5.py
@@ -0,0 +1,31 @@
+# -------------------------------------------------------------------
+# 
+# Excercise from: http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+
+# Objectives:
+# 1. Write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure # your program works on two lists of different sizes.
+# 2. Randomly generate two lists to test this
+# 3. Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+#
+# ------------------------------------------------------------------- 
+
+
+from random import randint
+
+def main():
+    x, y = rand_list(0, 10, 20), rand_list(0, 10, 20)
+    z = compare_lists(x, y)
+    print(z)
+
+def rand_list(min_value, max_value, max_elements):
+    my_list = []
+    size = randint(0, max_elements)
+    for element in range(size):
+        my_list.append(randint(min_value, max_value))
+    return my_list
+
+def compare_lists(x, y):
+    return list({element for element in x if element in y})
+
+if __name__ == "__main__":
+    main()
