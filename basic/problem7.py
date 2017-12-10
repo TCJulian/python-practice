213eef71e04ace9a1a23d2b3e5ad4b67d05aab81
Tyler C. Julian <tylercjulian@live.com>
2017-12-10 11:39:07 -0500

    Removed unnecessary bracket

--- basic/problem7.py
+++ basic/problem7.py
@@ -12,7 +12,7 @@
 
 def main():
     a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-    even = [num] for num in a if num % 2 == 0]
+    even = [num for num in a if num % 2 == 0]
     print(even)
 
 if __name__ == "__main__":
