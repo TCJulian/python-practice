4a620739188d66c68881ef6dd618716c98d18617
Tyler Julian <tcjulian1717@live.com>
2018-02-07 10:36:44 -0500

    Added description comment

--- basic/problem_15.py
+++ basic/problem_15.py
@@ -1,3 +1,12 @@
+# -------------------------------------------------------------------
+#
+# Excercise from: http://www.practicepython.org/exercise/2014/05/21/15-reverse-word-order.html
+#
+# Objectives:
+# 1. Write a program that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order.
+#
+# -------------------------------------------------------------------
+
 def main():
     text = input("Please provide a sentence to reverse:\n")
     text_list = text.split()
