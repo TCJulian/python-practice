3089cb724d53c25dae3b03895653ba88b901a11c
Tyler C. Julian <tylercjulian@live.com>
2018-03-02 11:13:27 -0500

    Implement main()

--- basic/problem_16.py
+++ basic/problem_16.py
@@ -1,7 +1,20 @@
 import random
 
 def main():
-	pass
+	done = False
+	while done == False:
+		length = int(input("How long does the password need to be? Please input an integer."))
+		strength = input("Do you want a strong password? Please input 'yes' or 'no'.")
+		
+		if strength == "yes":
+			password = password_maker(length)
+		else:
+			password = password_maker(length, strong=False)
+		print("Your password is '{}'.").format(password)
+		
+		reply = input("Would you like a different password? Please input 'yes' or 'no'.")
+		if reply == 'no':
+			done = True
 
 def password_maker(length, strong=True):
 	if strong:
