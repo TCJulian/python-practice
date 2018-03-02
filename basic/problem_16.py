b6eeb7fe64778c1ec25b8ccf60b3e87a9783138d
Tyler C. Julian <tylercjulian@live.com>
2018-03-02 11:02:04 -0500

    Implement password_maker()

--- basic/problem_16.py
+++ basic/problem_16.py
@@ -1,1 +1,23 @@
+import random
 
+def main():
+	pass
+
+def password_maker(length, strong=True):
+	if strong:
+		strong_chars = [chr(i) for i in range(3, 126)]
+		password_list = random.sample(strong_chars, length)
+		password = " ".join(password_list)
+	else:
+		weak_words = ["raisin", "airplane", "tree", "goat", "bicycle", "Steelers", 
+					  "barbie", "ant", "pal", "guy", "gal", "super", "special", 
+					  "Texas", "cheese", "bacon", "egg", "lipstick", "tootpaste"]
+		password_len = 0
+		while password_len < length:
+			random_word = random.sample(weak_words, 1)
+			password_len += len(random_word[0])
+			password = " ".join(random_word)
+	return password		
+
+if __name__ == "__main__":
+	main()
