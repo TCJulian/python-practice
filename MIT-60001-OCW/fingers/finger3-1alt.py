73198d71616e5817bc263ecfb4f974f9c2da6371
tcjulian <tylercjulian@live.com>
2020-11-29 10:45:05 -0600

    Add file description

--- MIT-60001-OCW/fingers/finger3-1alt.py
+++ MIT-60001-OCW/fingers/finger3-1alt.py
@@ -1,3 +1,9 @@
+### Finger 3-1 ###
+# Write a program that asks the user to enter an integer and
+# prints two integers, root and pwr, such that 0 < pwr < 6 and root**pwr is equal
+# to the integer entered by the user. If no such pair of integers exists, it should
+# print a message to that effect.
+
 i = int(input("Enter an integer: "))
 
 pairs = 0
