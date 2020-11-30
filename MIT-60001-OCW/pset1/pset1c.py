abb50372d7ff4d0d7ef8183d62164393cd8e6152
tcjulian <tylercjulian@live.com>
2020-11-30 12:46:03 -0600

    Lint

--- MIT-60001-OCW/pset1/pset1c.py
+++ MIT-60001-OCW/pset1/pset1c.py
@@ -3,7 +3,7 @@
 Created on Wed Dec 12 13:22:36 2018
 
 @author: Tyler
-"""  
+"""
 salary = float(input("Enter your annual salary: "))
 total_cost = 1000000
 semi_annual_raise = .07
@@ -23,12 +23,12 @@ while True:
     annual_salary = salary
     monthly_salary = annual_salary/12
     saving_rate = portion_saved/10000
-    
+
     for month in range(1, 37):
-            current_savings += monthly_salary*saving_rate + current_savings*r/12
-            if month%6 == 0:
-                annual_salary += annual_salary*semi_annual_raise
-                monthly_salary = annual_salary/12
+        current_savings += monthly_salary*saving_rate + current_savings*r/12
+        if month%6 == 0:
+            annual_salary += annual_salary*semi_annual_raise
+            monthly_salary = annual_salary/12
     if portion_saved == low or portion_saved == high:
         print("It is not possible to pay down the payment in three years.")
         break
@@ -45,4 +45,3 @@ while True:
     if steps > 1000:
         print("RUNTIME ERROR")
         break
-
