213bb6d76b0e3699eadc5247ef64179dfc61d3fc
tcjulian <tylercjulian@live.com>
2021-02-05 09:08:54 -0600

    Implement AndTrigger class

--- MIT-60001-OCW/pset5/ps5.py
+++ MIT-60001-OCW/pset5/ps5.py
@@ -194,7 +194,17 @@ class NotTrigger(Trigger):
         return not self.trigger.evaluate(story)
 
 # Problem 8
-# TODO: AndTrigger
+class AndTrigger(Trigger):
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+    def evaluate(self, story):
+        """
+        Takes in two trigger objects as arguments.
+        Returns True if both triggers occur in the story,
+        or False otherwise.
+        """
+        return self.trigger1.evaluate(story) and self.trigger2.evaluate(story)
 
 # Problem 9
 # TODO: OrTrigger
