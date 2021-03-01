da4ccc0a3b2b111cb52a01b4a73d492fc7acf6ef
tcjulian <tylercjulian@live.com>
2021-03-01 11:52:23 -0600

    Initialize problem_20

---
+++ basic/problem_20.py
@@ -0,0 +1,13 @@
+"""
+Exercise from:
+http://www.practicepython.org/exercise/2014/11/11/20-element-search.html
+
+Objective:
+Write a function that takes an ordered list of numbers (a list where the
+elements are in order from smallest to largest) and another number. The
+function decides whether or not the given number is inside the list and returns
+(then prints) an appropriate boolean.
+
+Extra:
+Use binary search.
+"""
