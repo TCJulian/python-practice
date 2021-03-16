cc1cac27f963a6ee54f062672c3e591b360cdf3d
tcjulian <tylercjulian@live.com>
2021-03-16 11:35:25 -0500

    p2 solution

---
+++ basic/w3resource/p2.py
@@ -0,0 +1,12 @@
+"""
+Exercise from:
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+Write a Python program to get the Python version you are using.
+"""
+import sys
+import os
+
+os.system('python3 -V')
+# or
+print(sys.version)
