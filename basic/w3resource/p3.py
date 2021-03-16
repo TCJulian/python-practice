380c668962193e88cc64808465b6cf45c8be46fa
tcjulian <tylercjulian@live.com>
2021-03-16 11:52:41 -0500

    Solution

--- basic/w3resource/p3.py
+++ basic/w3resource/p3.py
@@ -8,3 +8,7 @@ Sample Output :
     Current date and time :
     2014-07-05 14:34:14
 """
+from datetime import datetime
+
+print('Current date and time:')
+print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
