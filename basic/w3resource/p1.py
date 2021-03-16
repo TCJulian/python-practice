a40c31cad359bc217e42d109f305f691d792a7b6
tcjulian <tylercjulian@live.com>
2021-03-16 11:24:26 -0500

    Add exercise source

--- basic/w3resource/p1.py
+++ basic/w3resource/p1.py
@@ -1,4 +1,7 @@
 """
+Exercise from:
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
 Write a Python program to print the following string in a specific format (see the output). Go to the editor
 Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
 
