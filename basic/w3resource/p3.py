7e5af6ccd3acbbf2e0076d0bf87bd4edafdb6cf4
tcjulian <tylercjulian@live.com>
2021-03-16 11:38:20 -0500

    Initialize p3

---
+++ basic/w3resource/p3.py
@@ -0,0 +1,10 @@
+"""
+Exercise from:
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+Write a Python program to display the current date and time.
+
+Sample Output :
+    Current date and time :
+    2014-07-05 14:34:14
+"""
