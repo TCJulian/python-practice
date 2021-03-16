f547435442c9e7d70ead09cdfa8c0eb613b42ce7
tcjulian <tylercjulian@live.com>
2021-03-15 23:53:55 -0500

    p1 solution

--- basic/w3resource/p1.py
+++ basic/w3resource/p1.py
@@ -9,3 +9,12 @@ Twinkle, twinkle, little star,
 Twinkle, twinkle, little star, 
 	How I wonder what you are
 """
+print("""
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+"""
+)
