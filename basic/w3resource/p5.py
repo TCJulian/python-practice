fb80e75c1c58f3479b193ed4020ffe2a4e954a5e
tcjulian <tylercjulian@live.com>
2021-04-14 15:25:53 -0500

    Initialize p5

---
+++ basic/w3resource/p5.py
@@ -0,0 +1,7 @@
+"""
+Exercise from:
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+Write a Python program which accepts the user's first and last name and print
+them in reverse order with a space between them.
+"""
