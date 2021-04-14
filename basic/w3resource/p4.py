daf723552cc1759e3426ee0cfbf06a41167048a9
tcjulian <tylercjulian@live.com>
2021-04-14 15:06:33 -0500

    Initialize p4

---
+++ basic/w3resource/p4.py
@@ -0,0 +1,11 @@
+"""
+Exercise from:
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+Write a Python program which accepts the radius of a circle from the user and
+compute the area.
+
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504
+"""
