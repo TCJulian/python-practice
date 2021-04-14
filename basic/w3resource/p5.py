a0009213ebb8e8b51dafc2a9f7fdd209cba0f06a
tcjulian <tylercjulian@live.com>
2021-04-14 15:29:35 -0500

    Solution

--- basic/w3resource/p5.py
+++ basic/w3resource/p5.py
@@ -5,3 +5,11 @@ https://www.w3resource.com/python-exercises/python-basic-exercises.php
 Write a Python program which accepts the user's first and last name and print
 them in reverse order with a space between them.
 """
+
+def main():
+    first = input("What is your first name? ")
+    last = input("What is your last name? ")
+    print(f"Hello {last} {first}. I have reversed your name!")
+
+if __name__ == "__main__":
+    main()
