bca83adf997c79d102db7690625bc828120049bb
tcjulian <tylercjulian@live.com>
2021-04-14 15:24:02 -0500

    Solution

--- basic/w3resource/p4.py
+++ basic/w3resource/p4.py
@@ -9,3 +9,15 @@ Sample Output :
 r = 1.1
 Area = 3.8013271108436504
 """
+from math import pi
+
+def main():
+    try:
+        r = float(input("Enter the radius of your circle: "))
+        print(f"r = {r}")
+        print(f"Area =  {pi * r**2}")
+    except ValueError:
+        print("ERROR: Input must be a number.")
+
+if __name__ == "__main__":
+    main()
