e7f80e7e8759174fd75f9dee5ae8053f8241e223
tcjulian <tylercjulian@live.com>
2021-04-14 15:31:50 -0500

    Initialize p6

---
+++ basic/w3resource/p6.py
@@ -0,0 +1,18 @@
+"""
+Exercise from:
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+Write a Python program which accepts a sequence of comma-separated numbers
+from user and generate a list and a tuple with those numbers.
+
+Sample data : 3, 5, 7, 23
+
+Output :
+List : ['3', ' 5', ' 7', ' 23']
+Tuple : ('3', ' 5', ' 7', ' 23')
+"""
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
