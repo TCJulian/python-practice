7e7413aa646e611756a84efabaa06d64c89004a7
tcjulian <tylercjulian@live.com>
2021-05-03 13:06:35 -0500

    Initialize p7

---
+++ basic/w3resource/p7.py
@@ -0,0 +1,10 @@
+"""
+Exercise from:
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+Write a Python program to accept a filename from the user and print the
+extension of that.
+
+Sample filename : abc.java
+Output : java
+"""
