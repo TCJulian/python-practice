835d5b8d51854f5c758fd4fd299341cac0462f73
tcjulian <tylercjulian@live.com>
2021-05-03 13:21:06 -0500

    Initialize p8

---
+++ basic/w3resource/p8.py
@@ -0,0 +1,9 @@
+"""
+Exercise from:
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+Write a Python program to display the first and last colors from the following
+list.
+
+color_list = ["Red","Green","White" ,"Black"]
+"""
