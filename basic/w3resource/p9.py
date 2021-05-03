adb4b75100a7b2cf3b63cb18d0479d46b72f1f88
tcjulian <tylercjulian@live.com>
2021-05-03 13:30:16 -0500

    Solution for p9

--- basic/w3resource/p9.py
+++ basic/w3resource/p9.py
@@ -8,3 +8,5 @@ from exam_st_date).
 exam_st_date = (11, 12, 2014)
 Sample Output : The examination will start from : 11 / 12 / 2014
 """
+exam_st_date = (11, 12, 2014)
+print(f"The examination will start from: {exam_st_date[0]}/{exam_st_date[1]}/{exam_st_date[2]}")
